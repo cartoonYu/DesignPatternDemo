a2b4ad49d04bf291e8ef369e6e6bec5f5c158484
cartoon <cartoonyu3@gmail.com>
2021-07-18 22:00:47 +0800

    finish singleton pattern demo

---
+++ src/demo/singleton/HungryType.java
@@ -0,0 +1,18 @@
+package demo.singleton;
+
+/**
+ * hungry type singleton, init object when system start
+ * @author cartoon
+ * @version 1.0
+ * @since 2021/7/18 21:30
+ */
+public class HungryType {
+
+    private static HungryType instance = new HungryType();
+
+    public static HungryType getInstance(){
+        return instance;
+    }
+
+    private HungryType(){}
+}
---
+++ src/demo/singleton/StaticInnerType.java
@@ -0,0 +1,19 @@
+package demo.singleton;
+
+/**
+ * @author cartoon
+ * @version 1.0
+ * @since 2021/7/18 21:40
+ */
+public class StaticInnerType {
+
+    private static class StaticInnerHolder{
+        private static final StaticInnerType instance = new StaticInnerType();
+    }
+
+    private static StaticInnerType getInstance(){
+        return StaticInnerHolder.instance;
+    }
+
+    private StaticInnerType(){}
+}
