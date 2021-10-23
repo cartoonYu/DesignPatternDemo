eec1478fbf9739826a86fa65eb0e1c6981fe25cf
cartoon <cartoonyu3@gmail.com>
2021-10-23 16:03:42 +0800

    add iterator demo

---
+++ src/demo/BehavioralDesignPatterns/iterator/ArrayCollectionIterator.java
@@ -0,0 +1,26 @@
+package demo.BehavioralDesignPatterns.iterator;
+
+/**
+ * @author cartoon.yu
+ * @date 2021/10/23 15:56
+ */
+public class ArrayCollectionIterator implements Iterator{
+
+    private ArrayCollection arrayCollection;
+
+    private Integer index;
+
+    @Override
+    public boolean hasNext() {
+        return arrayCollection.size() > index;
+    }
+
+    @Override
+    public Object get() {
+        return arrayCollection.get(index++);
+    }
+
+    public ArrayCollectionIterator(ArrayCollection arrayCollection) {
+        this.arrayCollection = arrayCollection;
+    }
+}
