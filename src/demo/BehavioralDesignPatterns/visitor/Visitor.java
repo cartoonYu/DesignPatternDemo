35fcc1ff861b01393ab4d350b9e728f791f7eba5
cartoon <cartoonyu3@gmail.com>
2021-10-30 16:37:54 +0800

    add visitor demo

---
+++ src/demo/BehavioralDesignPatterns/visitor/Visitor.java
@@ -0,0 +1,12 @@
+package demo.BehavioralDesignPatterns.visitor;
+
+/**
+ * @author cartoon
+ * @date 2021/10/30 16:30
+ */
+public interface Visitor {
+
+    void visit(Resource1 resource1);
+
+    void visit(Resource2 resource2);
+}
---
+++ src/demo/BehavioralDesignPatterns/visitor/Visitor2.java
@@ -0,0 +1,18 @@
+package demo.BehavioralDesignPatterns.visitor;
+
+/**
+ * @author cartoon
+ * @date 2021/10/30 16:36
+ */
+public class Visitor2 implements Visitor{
+
+    @Override
+    public void visit(Resource1 resource1) {
+        System.out.println("visitor2 visit resource1");
+    }
+
+    @Override
+    public void visit(Resource2 resource2) {
+        System.out.println("visitor2 visit resource2");
+    }
+}
