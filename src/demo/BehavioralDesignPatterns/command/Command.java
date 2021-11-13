3fdc59cdd8ecda5d167e20b2e22fb33df4c9139c
cartoon <cartoonyu3@gmail.com>
2021-11-13 11:06:45 +0800

    add command pattern demo

---
+++ src/demo/BehavioralDesignPatterns/command/Command.java
@@ -0,0 +1,6 @@
+package demo.BehavioralDesignPatterns.command;
+
+public interface Command {
+
+    void execute();
+}
---
+++ src/demo/BehavioralDesignPatterns/command/DemoCommand1.java
@@ -0,0 +1,20 @@
+package demo.BehavioralDesignPatterns.command;
+
+/**
+ * @author cartoon
+ * @version 1.0
+ * @since 2021/11/13 11:02
+ */
+public class DemoCommand1 implements Command{
+
+    private CommandData commandData;
+
+    @Override
+    public void execute() {
+        // execute specific logic
+    }
+
+    public DemoCommand1(CommandData commandData) {
+        this.commandData = commandData;
+    }
+}
