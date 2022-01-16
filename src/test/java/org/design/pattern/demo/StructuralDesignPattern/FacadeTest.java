e01bfa9590cd9b88ca33927d5b5f53b194417ed0
cartoon <cartoonyu3@gmail.com>
2022-01-16 16:29:39 +0800

    add facade and fly weight test

---
+++ src/test/java/org/design/pattern/demo/StructuralDesignPattern/FacadeTest.java
@@ -0,0 +1,22 @@
+package org.design.pattern.demo.StructuralDesignPattern;
+
+import org.DesignPatternDemo.StructuralDesignPattern.facade.FacadeService;
+import org.DesignPatternDemo.StructuralDesignPattern.facade.FacadeServiceImpl;
+import org.design.pattern.demo.BaseTest;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author cartoon
+ * @date 2022/1/16 16:22
+ */
+@DisplayName("facade test")
+public class FacadeTest extends BaseTest {
+
+    @DisplayName("normal test")
+    @Test
+    public void normalTest(){
+        FacadeService facadeService = new FacadeServiceImpl();
+        facadeService.operate("test1", "test2");
+    }
+}
---
+++ src/test/java/org/design/pattern/demo/StructuralDesignPattern/FlyWeightTest.java
@@ -0,0 +1,21 @@
+package org.design.pattern.demo.StructuralDesignPattern;
+
+import org.DesignPatternDemo.StructuralDesignPattern.flyWeight.FlyWeightFactory;
+import org.DesignPatternDemo.StructuralDesignPattern.flyWeight.FlyWeightUnit;
+import org.design.pattern.demo.BaseTest;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author cartoon
+ * @date 2022/1/16 16:27
+ */
+@DisplayName("fly weight test")
+public class FlyWeightTest extends BaseTest {
+
+    @DisplayName("normal test")
+    @Test
+    public void normalTest(){
+        FlyWeightUnit flyWeightUnit = FlyWeightFactory.getFlyWeight(200);
+    }
+}
