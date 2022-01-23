8e224a4b7973841858b61a3af503eed523557ba9
cartoon <cartoonyu3@gmail.com>
2022-01-23 15:33:13 +0800

    add command and interpreter test

---
+++ src/test/java/org/design/pattern/demo/BehavioralDesignPatterns/CommandTest.java
@@ -0,0 +1,27 @@
+package org.design.pattern.demo.BehavioralDesignPatterns;
+
+import org.DesignPatternDemo.BehavioralDesignPatterns.command.Command;
+import org.DesignPatternDemo.BehavioralDesignPatterns.command.CommandData;
+import org.DesignPatternDemo.BehavioralDesignPatterns.command.DemoCommand1;
+import org.design.pattern.demo.BaseTest;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author cartoon
+ * @date 2022/1/22 17:30
+ */
+@DisplayName("command test")
+public class CommandTest extends BaseTest {
+
+    @DisplayName("normal test")
+    @Test
+    public void normalTest(){
+        CommandData commandData = new CommandData();
+        commandData.setUserId(111L)
+                .setToStatus(1)
+                .setFromStatus(0);
+        Command command = new DemoCommand1(commandData);
+        command.execute();
+    }
+}
---
+++ src/test/java/org/design/pattern/demo/BehavioralDesignPatterns/InterpreterTest.java
@@ -0,0 +1,26 @@
+package org.design.pattern.demo.BehavioralDesignPatterns;
+
+import org.DesignPatternDemo.BehavioralDesignPatterns.interpreter.AddExpression;
+import org.DesignPatternDemo.BehavioralDesignPatterns.interpreter.Expression;
+import org.DesignPatternDemo.BehavioralDesignPatterns.interpreter.NumberExpression;
+import org.design.pattern.demo.BaseTest;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author cartoon
+ * @date 2022/1/23 15:30
+ */
+@DisplayName("interpreter test")
+public class InterpreterTest extends BaseTest {
+
+
+    @DisplayName("normal test")
+    @Test
+    public void normalTest(){
+        Expression expression1 = new NumberExpression(2);
+        Expression expression2 = new NumberExpression(2);
+        Expression expression = new AddExpression(expression1, expression2);
+        expression.intercept();
+    }
+}
